075af11a2312bbafbafe307f16a79322c91ac382
josep <josep@PC>
2022-04-22 15:15:34 -0600

    El anexo de la clase Alumno

---
+++ src/labp1/LabP1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labp1;
+
+/**
+ *
+ * @author josep
+ */
+public class LabP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n",
+               "Bienvenido a", "la programacion en java");
+       
+    }
+    
+}
